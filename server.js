74eba2439018a78c9a935f2ffacdd96822a7ce4e
asathiya007 <akshay.sathiya@gmail.com>
2019-07-25 19:34:41 -0400

    completed /register route

--- server.js
+++ server.js
@@ -1,11 +1,62 @@
 const express = require("express");
 const cors = require("cors");
+const knex = require("knex");
+const bcrypt = require("bcrypt-nodejs");
+const bodyParser = require("body-parser");
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: '',
+        password: '',
+        database: 'hawkeye'
+    }
+});
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.json("this is working!");
-})
+    db.select("*").from("users")
+        .then(users => res.json(users))
+        .catch(err => res.status(500).json("error retrieving users data"));
+});
+
+app.post("/register", (req, res) => {
+    const {name, email, password} = req.body;
+
+    const hash = bcrypt.hashSync(password);
+
+    db.transaction(trx => {
+        return trx.insert({
+            email: email,
+            hash: hash
+        })
+            .into("login")
+            .returning("email")
+            .then(loginEmail => {
+                return trx.insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+                })
+                    .into("users")
+                    .returning("*")
+                    .then(user => {
+                        if (user[0]) {
+                            res.json(user[0]);
+                        } else {
+                            res.status(500).json("error storing user data");
+                        }
+                    })
+                    .catch(err => res.status(500).json("error storing user data"));
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    })
+        .catch(err => res.status(500).json("error registering new user"));
+});
 
 app.listen(3001);
